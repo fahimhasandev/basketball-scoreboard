c59fc077761f7ec19334928529a48261c6d56b5d
fahimhasandev <fhasan5@wgu.edu>
2024-08-11 23:41:28 -0500

    Initial setup

---
+++ js/main.js
@@ -0,0 +1,39 @@
+// get the text of one
+homeText = document.querySelector(".home-text")
+guestText = document.querySelector(".guest-text")
+homeBtnReset = document.querySelector(".home-btn-reset")
+guestBtnReset = document.querySelector(".guest-btn-reset")
+
+function homeAddOne() {
+  let getHomeText = +homeText.textContent
+  homeText.textContent = getHomeText + 1
+}
+function homeAddTwo() {
+  let getHomeText = +homeText.textContent
+  homeText.textContent = getHomeText + 2
+}
+function homeAddThree() {
+  let getHomeText = +homeText.textContent
+  homeText.textContent = getHomeText + 3
+}
+
+function guestAddOne() {
+  let guestHomeText = +guestText.textContent
+  guestText.textContent = guestHomeText + 1
+}
+function guestAddTwo() {
+  let guestHomeText = +guestText.textContent
+  guestText.textContent = guestHomeText + 2
+}
+
+function guestAddThree() {
+  let guestHomeText = +guestText.textContent
+  guestText.textContent = guestHomeText + 3
+}
+
+function resetForHome() {
+  homeText.textContent = 0
+}
+function resetForGuest() {
+  guestText.textContent = 0
+}
